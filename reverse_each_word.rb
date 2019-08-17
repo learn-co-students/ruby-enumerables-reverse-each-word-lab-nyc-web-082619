4a6798c7bd1d5a598053a9a3656f5da2bcce6dff
Thomas Castagna <castagna.tm@gmail.com>
2019-08-17 20:57:36 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
 def reverse_each_word(string)
-  array = string.split(" ")
-  new_string = (array.collect {|word| word.reverse!}).join(" ")
-  return new_string
+  string.split(" ").collect {|word| word.reverse!}.join(" ")
 end
